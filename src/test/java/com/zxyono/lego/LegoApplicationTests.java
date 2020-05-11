99fde26c20fa5e0a30d9a4b4edbc5bbf59e7ff6a
小野 <823734910@qq.com>
2020-05-11 13:30:54 +0800

    program init

---
+++ src/test/java/com/zxyono/lego/LegoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zxyono.lego;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LegoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
