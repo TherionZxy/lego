da7aea7f1d225033b661abba7db11d5d3c95ab3d
小野 <823734910@qq.com>
2020-05-11 17:29:04 +0800

    图片上传功能完成

--- src/main/java/com/zxyono/lego/LegoApplication.java
+++ src/main/java/com/zxyono/lego/LegoApplication.java
@@ -1,11 +1,12 @@
 package com.zxyono.lego;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.zxyono.lego.mapper")
 public class LegoApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(LegoApplication.class, args);
 	}
---
+++ src/main/resources/sql/create.sql
@@ -0,0 +1,4 @@
+CREATE TABLE IF NOT EXISTS `test`(
+  name varchar(32) primary key,
+  msg varchar(128)
+);
